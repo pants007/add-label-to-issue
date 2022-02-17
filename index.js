ea482242c2c0ac2e41ef12ce25b4858476fbfbe4
pants007 <qrw992@alumni.ku.dk>
2022-02-17 13:43:57 +0100

    testing
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const myToken = core.getInput('repo-token');
+  const octokit = github.getOctokit(myToken);
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
