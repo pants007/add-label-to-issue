aca5916100a32af98d635e285a1f44c12421e327
pants007 <qrw992@alumni.ku.dk>
2022-02-18 14:25:19 +0100

    blabla
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function addLabels(){
       }
     );
 
-    console.log(`The retrieved issue is:\n ${updatedIssue.data}`);
+    console.log(`The retrieved issue is:\n ${updatedIssue.data.id}`);
     console.log(`The card is created with \n
     column_id = ${column.id}\n
     note = ${newTitle.trim()}\n
