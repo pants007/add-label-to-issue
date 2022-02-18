5f7bd8c4f589d90814ad8337d372bac33db4a98e
pants007 <qrw992@alumni.ku.dk>
2022-02-18 12:02:53 +0100

    blabla
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ async function addLabels(){
       repository_id: repoId,
       q:`${labelTokens.join('+')}&repository_id=${repoId}`
     })
-    console.log(repoLabels.data.items);
+    console.log(repoLabels);
     //check if issue has changed since the action started
     var updatedIssue = await octokit.rest.issues.get({
       owner: ownerName,
