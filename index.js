daf694545813aeb44fcdd5d4283b0537d6078d84
pants007 <qrw992@alumni.ku.dk>
2022-02-19 15:45:18 +0100

    blabla
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function findItems(str, key, delim, seqDelim){
     return retDict;
 }
 
-async function addLabels(){
+async function main(){
     // `who-to-greet` input defined in action metadata file
     const myToken = core.getInput('repo-token');
     const issueTitle = core.getInput('issue-title');
@@ -50,11 +50,6 @@ async function addLabels(){
     const projectSubstring = projectStringAndTokens.string;
     const issueTitleWithoutLabels = issueTitle.replace(labelSubstring, '');
     const newTitle = issueTitleWithoutLabels.replace(projectSubstring, '');
-    
-    
-    if (labelTokens.length < 1){
-      throw 'No labels were found. Not doing anything'
-    }
 
     var repoLabels = await octokit.rest.search.labels({
       repository_id: repoId,
@@ -115,7 +110,94 @@ async function addLabels(){
     The issue was added to ${repoName}/projects/${project.name} in column \'${columnName}\'`;
 }
 
-addLabels().then(
+async function AddLabelsAutomaticProjectAssignment(){
+  const myToken = core.getInput('repo-token');
+  const issueTitle = core.getInput('issue-title');
+  const issueNumber = core.getInput('issue-number');
+  const columnName = core.getInput('column-name');
+  const repoName = github.context.payload.repository.name;
+  const repoId = github.context.payload.repository.id;
+  const ownerName = github.context.payload.repository.owner.login;
+  const octokit = github.getOctokit(myToken);
+
+  const labelStringAndTokens = findItems(issueTitle, 'labels', ',', ';');
+  const labelTokens = labelStringAndTokens.tokens;
+  
+  // remove the label substring from the issue title, makes it look better
+  
+  const projectStringAndTokens = findItems(issueTitle, 'project', ',', ';');
+  const projectTokens = projectStringAndTokens.tokens;
+  
+  const labelSubstring = labelStringAndTokens.string;
+  const projectSubstring = projectStringAndTokens.string;
+  const issueTitleWithoutLabels = issueTitle.replace(labelSubstring, '');
+  const newTitle = issueTitleWithoutLabels.replace(projectSubstring, '');
+
+  if (labelTokens.length < 1){
+    throw 'No labels were found. Not doing anything'
+  }
+
+  var repoLabels = await octokit.rest.search.labels({
+    repository_id: repoId,
+    q:`${labelTokens.join('+')}&repository_id=${repoId}`
+  })
+
+  //Check if the extracted tokens are valid labels in the repo
+  //Only add the valid labels, as we otherwise end up
+  //creating new labels unintentionally
+  labelsToAdd = [];
+  for(let repoLabel of repoLabels.data.items){
+    if (labelTokens.includes(repoLabel.name)){
+      labelsToAdd.push(repoLabel.name);
+    }
+  }
+  //check if issue has changed since the action started
+  var updatedIssue = await octokit.rest.issues.get({
+    owner: ownerName,
+    repo: repoName,
+    issue_number: issueNumber
+  });
+
+  await octokit.rest.issues.update({
+    owner: ownerName,
+    repo: repoName,
+    issue_number: issueNumber,
+    title: newTitle.trim(),
+    labels: labelsToAdd
+  });
+  core.setOutput('labels-added', labelsToAdd);
+
+  var projectsInfo = await octokit.rest.projects.listForRepo({
+    owner: ownerName,
+    repo: repoName
+  });
+
+  //only support assigning issue to single project
+  
+  const project = projectsInfo.data.find(project => project.name === projectTokens[0]);
+
+  if (project === undefined){
+    core.setOutput('project-name', 'None');
+    throw `Project \'${projectTokens[0]}\' does not exist in ${ownerName}/${repoName}.`
+  }
+  var projectColumns = await octokit.rest.projects.listColumns({project_id:project.id});
+  const column = projectColumns.data.find(column => column.name === columnName);
+
+  await octokit.rest.projects.createCard({
+      column_id:column.id,
+      content_id:updatedIssue.data.id,
+      content_type:'Issue'
+    }
+  );
+
+  console.log(`${updatedIssue.data.body}`);
+
+  core.setOutput('project-name', `${repoName}/projects/${project.name}`);
+  return `Added the labels \'${labelsToAdd}\' to issue #${issueNumber}\n
+  The issue was added to ${repoName}/projects/${project.name} in column \'${columnName}\'`;
+}
+
+main().then(
   result => {
     // eslint-disable-next-line no-console
     console.log(result);
