65665a58617de309c0b6b139be911b897d76fc10
pants007 <qrw992@alumni.ku.dk>
2022-02-19 19:38:11 +0100

    blabla
    Signed-off-by: pants007 <qrw992@alumni.ku.dk>

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ async function AddLabelsAutomaticProjectAssignment(){
     }
   }`);
 
-  console.log(cardQuery);
+  console.log(cardQuery.repository.projects.nodes);
   //only support assigning issue to single project
   const project = projectsInfo.data.find(project => project.name === projectName);
 
